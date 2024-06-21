5d203bb925f97d3ef20066742ae64bf3856c61ba
Babina081 <babitamoo333@gmail.com>
2024-06-21 21:44:32 +0545

    add tab feature in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,48 @@
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1> hello world</h1>
-      <Button >Button</Button>
-    </main>
+    <>
+      <section className="bg-white">
+        <div className="container flex items-center justify-between py-24">
+          <div>
+            <h1 className="text-7xl font-black font-sans ">
+              Delivering satisfaction everyday
+              <br />
+              <span className="text-primary">You call, we deliver !</span>
+            </h1>
+            <p className="text-2xl mt-8 max-w-lg leading-snug">
+              Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
+            </p>
+            <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">
+              Get Your Pizza Now
+            </Button>
+          </div>
+          <div>
+            <Image
+              src="/burger.svg"
+              alt="burger"
+              width={500}
+              height={500}
+            ></Image>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container py-12">
+          <Tabs defaultValue="pizza" className="w-[400px]">
+            <TabsList>
+              <TabsTrigger value="pizza" className="text-lg">Pizza</TabsTrigger>
+              <TabsTrigger value="beverages" className="text-lg">Beverages</TabsTrigger>
+            </TabsList>
+            <TabsContent value="pizza">Pizza List</TabsContent>
+            <TabsContent value="beverages">Beverages List</TabsContent>
+          </Tabs>
+        </div>
+      </section>
+    </>
   );
 }
