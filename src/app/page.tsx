14e42e35088aa7560c1106c59467427e2fa53d2a
Babina081 <babitamoo333@gmail.com>
2024-06-21 13:11:24 +0545

    add shadcn button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <main>hello world</main>;
+  return (
+    <main>
+      <h1> hello world</h1>
+      <Button variant="outline">Button</Button>
+    </main>
+  );
 }
