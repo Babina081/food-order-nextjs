4028db11ba2c645c5543d3c36a78f611a2ed584e
Babina081 <babitamoo333@gmail.com>
2024-06-21 15:06:23 +0545

    add phone and cart icon from lucid icons

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -7,13 +7,22 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import Link from "next/link";
+import { Phone, ShoppingBasket } from "lucide-react";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
     <header className="bg-white">
-      <nav className="container py-5">
+      <nav className="container py-5 flex items-center justify-between ">
         <div className="flex items-center space-x-4">
-          <Image src="food-logo.svg" alt="logo" height={90} width={130}></Image>
+          <Image
+            src="food-logo.svg"
+            alt="logo"
+            height={90}
+            width={130}
+            className="bg-transparent"
+          ></Image>
           <Select>
             <SelectTrigger className="w-[180px] focus:ring-0">
               <SelectValue placeholder="Select Restaurant" />
@@ -24,6 +33,33 @@ const Header = () => {
               <SelectItem value="kids-corner">Kids Corner</SelectItem>
             </SelectContent>
           </Select>
+        </div>
+        <div className="flex items-center gap-4">
+          <ul className="flex items-center font-medium  space-x-4">
+            <li>
+              <Link className="hover:text-primary" href={"/"}>
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link className="hover:text-primary" href={"/"}>
+                Orders
+              </Link>
+            </li>
+          </ul>
+          <div className="relative">
+            <Link href="/cart">
+              <ShoppingBasket className="hover:text-primary" />
+            </Link>
+            <span className="absolute -top-4 -right-4 items-center flex justify-center rounded-full bg-primary font-bold text-white w-6 h-6">
+              3
+            </span>
+          </div>
+          <div className="flex items-center ml-12">
+            <Phone />
+            <span>+977 9812345678</span>
+          </div>
+          <Button size={"sm"}>Logout</Button>
         </div>
       </nav>
     </header>
