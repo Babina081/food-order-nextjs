78c2de46b9a9ee688b0ab0f323bdc618f8169fd5
Babina081 <babitamoo333@gmail.com>
2024-06-25 09:32:56 +0545

    update header position fixed and rearranged the ui

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -13,7 +13,7 @@ import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <header className="bg-white">
+    <header className="bg-white fixed z-1 w-full mx-auto  ">
       <nav className="container py-5 flex items-center justify-between ">
         <div className="flex items-center space-x-4">
           <Image
@@ -23,34 +23,24 @@ const Header = () => {
             width={130}
             className="bg-transparent"
           ></Image>
-          <Select>
-            <SelectTrigger className="w-[180px] focus:ring-0">
-              <SelectValue placeholder="Select Restaurant" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="cheesy-delight">Cheesy Delight</SelectItem>
-              <SelectItem value="pizza-hut">Pizza Hut</SelectItem>
-              <SelectItem value="kids-corner">Kids Corner</SelectItem>
-            </SelectContent>
-          </Select>
         </div>
         <div className="flex items-center gap-4">
           <ul className="flex items-center font-medium  space-x-4">
-          <li>
+            <li>
               <Link className="hover:text-primary" href={"/home"}>
                 Home
               </Link>
-            </li> 
+            </li>
             <li>
               <Link className="hover:text-primary" href={"/about"}>
                 About
               </Link>
-            </li> 
+            </li>
             <li>
               <Link className="hover:text-primary" href={"/contact"}>
                 Contact
               </Link>
-            </li> 
+            </li>
             <li>
               <Link className="hover:text-primary" href={"/menu"}>
                 Menu
@@ -62,6 +52,20 @@ const Header = () => {
               </Link>
             </li>
           </ul>
+
+          <Select>
+            <SelectTrigger className="w-[180px] focus:ring-0">
+              <SelectValue placeholder="Select Restaurant" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="cheesy-delight">Cheesy Delight</SelectItem>
+              <SelectItem value="pizza-hut">Pizza Hut</SelectItem>
+              <SelectItem value="kids-corner">Kids Corner</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <Button size={"sm"}>Sign In</Button>
+          <Button size={"sm"}>Sign Up</Button>
           <div className="relative">
             <Link href="/cart">
               <ShoppingBasket className="hover:text-primary" />
@@ -70,11 +74,6 @@ const Header = () => {
               3
             </span>
           </div>
-          <div className="flex items-center ml-12">
-            <Phone />
-            <span>+977 9812345678</span>
-          </div>
-          <Button size={"sm"}>Logout</Button>
         </div>
       </nav>
     </header>
