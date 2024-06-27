f671e07eadbe5412ccdea8eacac6526dec9604a6
Babina081 <babitamoo333@gmail.com>
2024-06-27 19:33:26 +0545

    remove unwanted commented codes

--- src/app/contact/components/contact-card.tsx
+++ src/app/contact/components/contact-card.tsx
@@ -1,14 +1,9 @@
 "use client";
 import { contactInfo } from "@/data/contact_info";
 import { Mail, MapPin, Phone } from "lucide-react";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const ContactCard = () => {
-  //   const [isShaking, setisShaking] = useState(false);
-  //   useEffect(() => {
-  //     setisShaking(!isShaking);
-  //   }, []);
-
   return (
     <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-center ">
       {contactInfo.map((info, id) => {
