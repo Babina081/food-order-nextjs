f6ce5dc78583e58e4f319ee7c0556d8cc422f2f6
Babina081 <babitamoo333@gmail.com>
2024-06-27 09:27:16 +0545

    add separate file for food menu list

---
+++ src/data/food_list.ts
@@ -0,0 +1,46 @@
+import { Product } from "@/app/(home)/components/product-card";
+
+export const products: Product[] = [
+    {
+      id: "1",
+      name: "Margarita Pizza",
+      description: "This is a very tasty pizza",
+      image: "/burger.png",
+      price: 500,
+    },
+    {
+      id: "2",
+      name: "Margarita Pizza",
+      description: "This is a very tasty pizza",
+      image: "/burger.png",
+      price: 500,
+    },
+    {
+      id: "3",
+      name: "Margarita Pizza",
+      description: "This is a very tasty pizza",
+      image: "/burger.png",
+      price: 500,
+    },
+    {
+      id: "4",
+      name: "Margarita Pizza",
+      description: "This is a very tasty pizza",
+      image: "/burger.png",
+      price: 500,
+    },
+    {
+      id: "5",
+      name: "Margarita Pizza",
+      description: "This is a very tasty pizza",
+      image: "/burger.png",
+      price: 500,
+    },
+    {
+      id: "6",
+      name: "Margarita Pizza",
+      description: "This is a very tasty pizza",
+      image: "/burger.png",
+      price: 500,
+    },
+  ];
