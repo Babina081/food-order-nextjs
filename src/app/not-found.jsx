cb375992d2b0884495f19502d306f1b83e1a3d42
Babina081 <babitamoo333@gmail.com>
2024-06-27 09:31:43 +0545

    add error , loading and not-found file as per the file conventions of nextjs documentation

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <section className="absolute top-20 bg-red-500 z-50 text-black">NotFound</section>
+  );
+};
+
+export default NotFound;
