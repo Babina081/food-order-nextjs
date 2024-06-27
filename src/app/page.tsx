a0a321cd68c0fdbbe4db26991114b77b25acd8fa
Babina081 <babitamoo333@gmail.com>
2024-06-27 10:24:22 +0545

    update ui of home page banner section responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,15 @@ import { products } from "@/data/food_list";
 export default function Home() {
   return (
     <>
-      <section className="bg-white  py-44 relative z-0 container">
-        <div className="flex flex-col lg:flex-row items-center justify-between overflow-hidden gap-4">
-          <div className=" text-center lg:text-left">
-            <h1 className="text-5xl xl:text-6xl font-black font-sans ">
+      <section className=" pt-[3rem] pb-[3rem] relative z-0 container  ">
+        <div className="flex flex-col md:flex-row items-center justify-between overflow-hidden gap-4 w-full">
+          <div className=" text-center md:text-left  w-auto md:w-2/4  lg:w-[60%] ">
+            <h1 className="text-5xl lg:text-6xl xl:text-7xl font-black font-sans ">
               Delivering satisfaction everyday
               <br />
-              <span className="text-primary">
-                You call, we deliver !
-              </span>
+              <span className="text-primary">You call, we deliver !</span>
             </h1>
-            <p className="text-2xl mt-8 max-w-lg leading-snug">
+            <p className="text-lg lg:text-2xl mt-8 max-w-lg leading-snug">
               Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
             </p>
             <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">
@@ -27,13 +25,14 @@ export default function Home() {
             </Button>
           </div>
 
-          <div className="">
+          <div className=" w-auto md:w-2/4  lg:w-[40%]">
             {/* <CarouselBannerImage /> */}
             <Image
               src="/burger.svg"
               alt="burger"
-              width={500}
-              height={500}
+              width={400}
+              height={400}
+              className="w-full h-full  "
             ></Image>
           </div>
         </div>
