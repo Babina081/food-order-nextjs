47138f4d902d4e637dc78d6e8d38d1e86065f0e2
Babina081 <babitamoo333@gmail.com>
2024-06-27 17:42:45 +0545

    add loading burger

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,30 @@
+"use cient";
 import React from "react";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="load  h-[100vh] w-[100vw] flex flex-col justify-center items-center overflow-hidden">
+      <div className="hamburger">
+        <div className="top-bun"></div>
+        <div className="pickle"></div>
+        <div className="pickle"></div>
+        <div className="tomato">
+          <div></div>
+        </div>
+        <div className="tomato">
+          <div></div>
+        </div>
+
+        <div className="cheese"></div>
+        <div className="cheese"></div>
+
+        <div className="beef"></div>
+
+        <div className="bottom-bun"></div>
+      </div>
+      <h1 className="ham-h1">Loading...</h1>
+    </div>
+  );
 };
 
 export default Loading;
