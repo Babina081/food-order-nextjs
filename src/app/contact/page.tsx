a6c704141f1475b6a2791163b7dea07c477766ae
Babina081 <babitamoo333@gmail.com>
2024-06-27 19:31:21 +0545

    add contents of contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import ContactCard from "./components/contact-card";
 
 const ContactPage = () => {
   return (
-    <section className="container flex flex-col gap-4 w-auto pt-10 h-screen">
+    <section className="container flex flex-col gap-4 w-auto py-10 h-auto">
       {" "}
       <h1 className="ham-h1  font-semibold text-2xl text-center border-b-2 border-primary w-auto mx-auto mb-8 text-primary">
         Contact Us
       </h1>
+      <div className="flex flex-col ">
+        <ContactCard></ContactCard>
+        <div className="bg-green-300">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 };
