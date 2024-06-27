d77d2b13f1b7faae3b77f96922043547592a7034
Babina081 <babitamoo333@gmail.com>
2024-06-27 19:32:16 +0545

    add contact card component

---
+++ src/app/contact/components/contact-card.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { contactInfo } from "@/data/contact_info";
+import { Mail, MapPin, Phone } from "lucide-react";
+import React, { useEffect, useState } from "react";
+
+const ContactCard = () => {
+  //   const [isShaking, setisShaking] = useState(false);
+  //   useEffect(() => {
+  //     setisShaking(!isShaking);
+  //   }, []);
+
+  return (
+    <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-center ">
+      {contactInfo.map((info, id) => {
+        return (
+          <div
+            key={id}
+            className={`border border-primary/30 flex flex-col items-center rounded-xl p-4 shadow-md shadow-primary/30'}`}
+          >
+            {info.icon === "MapPin" && (
+              <MapPin
+                className={`h-14 w-10 text-primary gentle-hover-shake   `}
+              />
+            )}{" "}
+            {info.icon === "Mail" && (
+              <Mail className={`h-14 w-10 text-primary gentle-hover-shake `} />
+            )}{" "}
+            {info.icon === "Phone" && (
+              <Phone className={`h-14 w-10 text-primary gentle-hover-shake `} />
+            )}
+            <h3 className="text-2xl font-bold text-primary ">{info.title}</h3>
+            <p className="text-sm text-black">{info.content}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ContactCard;
