2549b1d1a2bd0c990cfe48a7d0b5f24b1862a96d
Babina081 <babitamoo333@gmail.com>
2024-06-27 19:29:14 +0545

    add contact info details list

---
+++ src/data/contact_info.ts
@@ -0,0 +1,23 @@
+type ContactInfo = {
+  icon: string;
+  title: string;
+  content: string;
+}[];
+
+export const contactInfo: ContactInfo = [
+  {
+    icon: "MapPin",
+    title: "Address",
+    content: "NewRoad, Pokhara",
+  },
+  {
+    icon: "Mail",
+    title: "Email",
+    content: "foodorder@gmail.com",
+  },
+  {
+    icon: "Phone",
+    title: "Phone Number",
+    content: "(+977) 9812345678",
+  },
+];
