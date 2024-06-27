fbbfe4c7040f4de60d211bb76cc3ced5743fde23
Babina081 <babitamoo333@gmail.com>
2024-06-27 17:05:47 +0545

    add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <section className="container flex flex-col gap-4 w-auto pt-10 h-screen">
+      {" "}
+      <h1 className="font-semibold text-2xl text-center border-b-2 border-primary w-auto mx-auto mb-8 text-primary">
+        Contact Us
+      </h1>
+    </section>
+  );
+};
+
+export default ContactPage;
