2aeb84e6c9846ac20f7c6ecad25980e1c54687c3
Babina081 <babitamoo333@gmail.com>
2024-06-28 21:16:23 +0545

    add faq list

---
+++ src/data/faqs_list.ts
@@ -0,0 +1,66 @@
+type FAQ = { question: string; answer: string };
+export const faqsInfo: FAQ[] = [
+  {
+    question: "Does Food-Order prepare food itself?",
+    answer:
+      "No. Food-Order is a food ordering and delivery service. We deliver food from any of the member restaurant.",
+  },
+  {
+    question: "From which restaurants can I order?",
+    answer:
+      "You can order from any of our partner restaurants. We already have more than 800+ most popular restaurants in our list, and the number will continue to grow day after day.",
+  },
+  {
+    question: "To which locations do you deliver?",
+    answer:
+      "We deliver to any locations inside of ring road and its periphery. We also have extended our services at Pokhara and Bharatpur.",
+  },
+  {
+    question: "Do you charge more for the food than in the restaurant?",
+    answer:
+      "No. The price in our online menu is exactly the same as the table menu price of the respective member restaurants.",
+  },
+  {
+    question: "What is the delivery charge?",
+    answer: "Please Click here to see the details regarding delivery fee.",
+  },
+  {
+    question: "How do I pay for my food?",
+    answer:
+      "You will pay using different digital payment methods as well as in cash to our delivery agent upon delivery of food to your location.",
+  },
+  {
+    question: "Do you guarantee the quality for food?",
+    answer:
+      "We make an effort to include restaurants on our website that are known for having good quality food. However, our main focus is hassle free ordering and delivery. The restaurant you choose will be responsible for the quality of food they offer.",
+  },
+  {
+    question: "Can I pick up the order myself from the restaurant?",
+    answer: "Sorry. This option is disabled at the moment.",
+  },
+  {
+    question: "Can I cancel my order if needed?",
+    answer:
+      "You can. However, you need to inform us over the phone on time. If the ordered food is already prepared by the restaurant, you cannot cancel the order.",
+  },
+  {
+    question: "Can I choose food from more than one restaurant at a time?",
+    answer:
+      "You can. However, you need to place different orders and meet the minimum amount for each restaurant.",
+  },
+  {
+    question: "What is the minimum order amount for delivery service?",
+    answer:
+      "We do not have a minimum order amount limit. However, we do charge a small order fee for amount less than Rs. 500.",
+  },
+  {
+    question: "What are your service hours?",
+    answer:
+      "For the time being, our service operates from 8:00 am and we take orders till 10:00 pm.",
+  },
+  {
+    question: "How much time does it take to get my food delivered?",
+    answer:
+      "Generally it takes 45 minutes to one hour to deliver your order. The time it takes depends on several factors like the distance between your location and the restaurant you ordered from, traffic conditions, how busy our delivery boys are, or how busy the restaurant is at particular point of time, the type of food and size of your order etc. We do our best to deliver your order as soon as possible. In order to make sure that the food is delivered on time, you may order food well in advance, even a few hours before the time you want your food to be delivered.",
+  },
+];
