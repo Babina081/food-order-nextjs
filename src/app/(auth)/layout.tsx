57c80a29a455e46ecadb507c5406bd73a869fa78
Babina081 <babitamoo333@gmail.com>
2024-06-29 22:37:13 +0545

    add auth files including login and signup page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Image from "next/image";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="container border-2 border-black w-full max-w-6xl min-h-[100vh] grid grid-cols-2 gap-2 overflow-hidden rounded-xl ">
+      <div className=" h-full w-full">
+        <Image
+          src={"/auth-food-bg.jpg"}
+          alt="food image"
+          width={1000}
+          height={1000}
+          className="object-cover w-full h-full"
+        ></Image>
+      </div>
+      <>{children}</>
+    </div>
+  );
+};
+
+export default AuthLayout;
---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginPage from "./login/page";
+
+const AuthPage = () => {
+  return <div className="container border-2 border-black w-full max-w-3xl "><LoginPage></LoginPage></div>;
+};
+
+export default AuthPage;
