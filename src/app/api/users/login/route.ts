e93568107ccf06afa22e981105d8106c65aa8cb0
Babina081 <babitamoo333@gmail.com>
2024-07-11 23:00:54 +0545

    add functionality of api login post request

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,4 +1,54 @@
 import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextResponse, NextRequest } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "User does not exists",
+        },
+        { status: 400 }
+      );
+    }
+
+    // check if password is correct
+    const validPassword = await bcryptjs.compare(password, user.passwordHash);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    const tokenData = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+    const response = NextResponse.json({
+      message: "Login Successful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, { httpOnly: true });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
