ff8cebd798b9fa2899003a7db34a9d72cdda828b
Babina081 <babitamoo333@gmail.com>
2024-07-29 08:45:00 +0545

    update restaurant type

--- src/components/custom/menu/restaurants/restaurant.tsx
+++ src/components/custom/menu/restaurants/restaurant.tsx
@@ -7,23 +7,28 @@ import { Search } from "lucide-react";
 import ProgressBar from "../../progress_bar";
 import ProgressCircle from "../../progress_circle";
 import SearchForm from "./searchForm";
+import { RestaurantInfoType } from "@/app/(home)/menu/page";
 
-export const Restaurant = () => {
+interface Props {
+  restaurants: RestaurantInfoType[];
+}
+
+export const Restaurant: React.FC<Props> = ({ restaurants }) => {
   const [filteredRestaurants, setFilteredRestaurants] =
-    useState(restaurantsInfo);
-  const handleSearch = (query: string) => {
-    setFilteredRestaurants(
-      restaurantsInfo.filter((resInfo) =>
-        resInfo.name.toLowerCase().includes(query.toLowerCase())
-      )
-    );
-  };
+    useState<RestaurantInfoType[]>(restaurants);
+  // const handleSearch = (query: string) => {
+  //   setFilteredRestaurants(
+  //     restaurantsInfo.filter((resInfo) =>
+  //       resInfo.name.toLowerCase().includes(query.toLowerCase())
+  //     )
+  //   );
+  // };
 
   return (
     <section className="">
       <ProgressBar />
       <ProgressCircle />
-      <SearchForm onSearch={handleSearch} />
+      {/* <SearchForm onSearch={handleSearch} /> */}
       <div className="pb-12 ">
         <Tabs defaultValue="all" className="  ">
           <div className="overflow-x-auto flex flex-nowrap ">
@@ -47,15 +52,17 @@ export const Restaurant = () => {
           <>
             <TabsContent value="all">
               <div className="grid  grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 mt-6">
-                {!handleSearch &&
+                {/* {
                   restaurantsInfo.map((resInfo) => {
                     return (
-                      <RestaurantCard
-                        key={resInfo.id}
-                        resInfo={resInfo}
-                      ></RestaurantCard>
+                      <>
+                        <RestaurantCard
+                          key={resInfo.id}
+                          resInfo={resInfo}
+                        ></RestaurantCard>
+                      </>
                     );
-                  })}
+                  })} */}
                 {filteredRestaurants.map((resInfo) => {
                   return (
                     <RestaurantCard
@@ -66,7 +73,7 @@ export const Restaurant = () => {
                 })}
               </div>
             </TabsContent>
-            <TabsContent value="delivery">
+            {/* <TabsContent value="delivery">
               <div className="grid  grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 mt-6">
                 {!handleSearch &&
                   restaurantsInfo
@@ -138,7 +145,7 @@ export const Restaurant = () => {
                   );
                 })}
               </div>
-            </TabsContent>
+            </TabsContent> */}
           </>
         </Tabs>
       </div>
