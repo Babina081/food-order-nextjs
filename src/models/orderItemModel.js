17c614976ec88420194998323114f52f831ec881
Babina081 <babitamoo333@gmail.com>
2024-08-20 17:19:10 +0545

    remove db

--- src/models/orderItemModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const orderItemSchema = mongoose.Schema({
-  menu: { type: mongoose.Schema.Types.ObjectId, ref: "Menu", required: true },
-  quantity: { type: Number, required: true },
-});
-module.exports = mongoose.model("OrderItem", orderItemSchema);
