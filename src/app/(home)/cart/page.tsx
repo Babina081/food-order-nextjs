fa4bb2ba32ff4d04ebdfdc534456453cc643bcea
Babina081 <babitamoo333@gmail.com>
2024-08-23 22:03:46 +0545

    add fetch cart items from redux store

--- src/app/(home)/cart/page.tsx
+++ src/app/(home)/cart/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Table,
@@ -8,11 +9,21 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Minus, Plus } from "lucide-react";
+import { Minus, Plus, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
+// import { useDispatch, useSelector } from "react-redux";
+import { remove } from "@/lib/store/features/cart/cartSlice";
 
 const CartPage = () => {
+  const dispatch = useAppDispatch();
+  const products = useAppSelector((state) => state.cart.items);
+  console.log("list......", products);
+  const handleRemove = (productId: string) => {
+    dispatch(remove(productId));
+  };
+
   return (
     <section className="container flex flex-col gap-4 w-full py-10 h-full">
       <h1 className=" font-bold text-3xl text-center border-b-2 border-primary  w-auto mx-auto mb-8 text-primary ">
@@ -27,34 +38,48 @@ const CartPage = () => {
 
             <TableHead className="w-1/5 text-center">Quantity</TableHead>
             <TableHead className="text-right w-1/5">Amount</TableHead>
+            <TableHead className="text-right w-1/5">Action</TableHead>
           </TableRow>
         </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">
-              {" "}
-              <Image
-                src="/food1.png"
-                alt="food image"
-                width={50}
-                height={50}
-                className="border border-primary"
-              />
-            </TableCell>
-            <TableCell className="font-medium">INV001</TableCell>
+        {products.map((product, index) => {
+          return (
+            <TableBody key={index}>
+              <TableRow>
+                <TableCell className="font-medium">
+                  {" "}
+                  <Image
+                    src={product.image || "/burger.png"}
+                    alt={product.name}
+                    width={50}
+                    height={50}
+                    className="border border-primary rounded-lg object-cover object-center"
+                  />
+                </TableCell>
+                <TableCell className="font-medium">{product.name}</TableCell>
 
-            <TableCell className="flex justify-between items-center">
-              <Button className="bg-green-500 hover:bg-green-300 hover:opacity-55">
-                <Plus />
-              </Button>
-              <div>1</div>
-              <Button variant={"destructive"} className="hover:opacity-55">
-                <Minus />
-              </Button>
-            </TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
-        </TableBody>
+                <TableCell className="flex justify-between items-center">
+                  <Button className="bg-green-500 hover:bg-green-300 hover:opacity-55">
+                    <Plus />
+                  </Button>
+                  <div>1</div>
+                  <Button variant={"destructive"} className="hover:opacity-55">
+                    <Minus />
+                  </Button>
+                </TableCell>
+                <TableCell className="text-right">${product.price}</TableCell>
+                <TableCell className="text-right">
+                  {" "}
+                  <Button
+                    className="btn rounded-full"
+                    onClick={() => handleRemove(product.id)}
+                  >
+                    <X className=" " />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          );
+        })}
         <TableFooter>
           <TableRow>
             <TableCell colSpan={3}>Total</TableCell>
