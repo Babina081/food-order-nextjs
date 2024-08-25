005a05beca978c66c8dff66a94dbf285c78b5315
Babina081 <babitamoo333@gmail.com>
2024-08-25 09:16:58 +0545

    add favorite slice

---
+++ src/lib/store/features/favorite/favoriteSlice.ts
@@ -0,0 +1,28 @@
+import { RestaurantInfoType } from "@/app/(home)/restaurants/page";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+interface FavoritesState {
+  favorites: RestaurantInfoType[];
+}
+
+const initialState: FavoritesState = {
+  favorites: [],
+};
+
+const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState,
+  reducers: {
+    addFavorite: (state, action: PayloadAction<RestaurantInfoType>) => {
+      state.favorites.push(action.payload);
+    },
+    removeFavorite: (state, action: PayloadAction<string>) => {
+      state.favorites = state.favorites.filter(
+        (res) => res.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addFavorite, removeFavorite } = favoritesSlice.actions;
+
+export default favoritesSlice.reducer;
