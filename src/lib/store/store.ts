7268ac453427c881b5ed573407ac32dbf29c2315
Babina081 <babitamoo333@gmail.com>
2024-08-25 09:17:26 +0545

    update store reduc persist functionality

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,31 +1,46 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
 import productReducer from "./features/product/productSlice";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-const persistConfig = {
-  key: "root",
-  storage: storage,
-};
+import favoriteReducer from "./features/favorite/favoriteSlice";
+import { persistStore } from "redux-persist";
 
 const rootReducer = combineReducers({
   cart: cartReducer,
   product: productReducer,
+  favorite: favoriteReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const makeStore = () => {
-  return configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({ serializableCheck: false }),
-  });
+  if (typeof window === "undefined") {
+    // Server-side: no persistence
+    return configureStore({
+      reducer: rootReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false }),
+    });
+  } else {
+    // Client-side: with persistence
+    const { persistReducer, persistStore } = require("redux-persist");
+    const storage = require("redux-persist/lib/storage").default;
+
+    const persistConfig = {
+      key: "root",
+      storage: storage,
+    };
+
+    const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+    return configureStore({
+      reducer: persistedReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false }),
+    });
+  }
 };
 
 export const store = makeStore();
-export const persistor = persistStore(store);
+export const persistor =
+  typeof window !== "undefined" ? persistStore(store) : null;
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
