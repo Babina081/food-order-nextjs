1f3b314bd06ab9cf6a27f8f1897173176c495389
Babina081 <babitamoo333@gmail.com>
2024-08-25 09:14:06 +0545

    add available unavailable restaurant status component

--- src/components/custom/menu/restaurants/open_close_status.tsx
+++ src/components/custom/menu/restaurants/open_close_status.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 
-const OpenCloseStatus = () => {
+interface OpenCloseStatusProps {
+  onFilterChange: (isAvailable: boolean | null) => void;
+}
+
+const OpenCloseStatus = ({ onFilterChange }: OpenCloseStatusProps) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    if (value === "open") {
+      onFilterChange(true);
+    } else if (value === "closed") {
+      onFilterChange(false);
+    } else {
+      onFilterChange(null); // Clear filter if "all" is selected
+    }
+  };
   return (
     <ul className="flex flex-col gap-3 ">
       <li>
         <div className="flex gap-2 items-center">
           <input
             type="radio"
-            name=""
+            name="availability"
             value="open"
             id="open"
             className="w-4 h-4"
+            onChange={handleChange}
           />
           <label htmlFor="open">Available</label>
         </div>
@@ -19,12 +34,26 @@ const OpenCloseStatus = () => {
         <div className="flex gap-2 items-center">
           <input
             type="radio"
-            name=""
+            name="availability"
             value="closed"
             id="closed"
             className="w-4 h-4"
+            onChange={handleChange}
           />
           <label htmlFor="closed">Unavailable</label>
+        </div>
+      </li>
+      <li>
+        <div className="flex gap-2 items-center">
+          <input
+            type="radio"
+            name="availability"
+            value="all"
+            id="all"
+            className="w-4 h-4"
+            onChange={handleChange}
+          />
+          <label htmlFor="all">All</label>
         </div>
       </li>
     </ul>
