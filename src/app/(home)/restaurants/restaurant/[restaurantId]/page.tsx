f74db77572afcdb9aaf64daf91f2e439a48a6611
Babina081 <babitamoo333@gmail.com>
2024-08-26 14:13:34 +0545

    add order cart item and tab toggle

--- src/app/(home)/restaurants/restaurant/[restaurantId]/page.tsx
+++ src/app/(home)/restaurants/restaurant/[restaurantId]/page.tsx
@@ -1,5 +1,6 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Collapsible,
@@ -25,8 +26,7 @@ import {
 
 import Link from "next/link";
 import {
-  ChevronsDown,
-  ChevronsUp,
+  CircleX,
   ImageIcon,
   Map,
   Plus,
@@ -34,21 +34,106 @@ import {
 } from "lucide-react";
 import { restaurantsInfo } from "@/data/restaurants_list";
 import Image from "next/image";
+import { FaSortDown, FaSortUp } from "react-icons/fa";
 
 const RestaurantDetails = ({
   params,
 }: {
   params: { restaurantId: string };
 }) => {
+  const [openSections, setOpenSections] = useState<Record<string, boolean>>({});
+  const [orderItems, setOrderItems] = useState<{
+    [key: string]: { quantity: number; price: number };
+  }>(() => {
+    // Get initial state from local storage
+    const savedOrderItems = localStorage.getItem("orderItems");
+    return savedOrderItems ? JSON.parse(savedOrderItems) : {};
+  });
+  const sectionRefs = useRef<Record<string, HTMLDivElement | null>>({});
+
   const restaurant = restaurantsInfo.find(
     (restaurant) => restaurant.id === Number(params?.restaurantId)
   );
+
+  // Save orderItems to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("orderItems", JSON.stringify(orderItems));
+  }, [orderItems]);
 
   if (!restaurant) {
     return <p>Restaurant not found.</p>;
   }
 
   const { name, address, minOrder, locationUrl, menuLists } = restaurant;
+
+  // Function to toggle the open stateconst handleToggle = (category: string) => {
+  const handleToggle = (category: string) => {
+    setOpenSections((prevState) => ({
+      ...prevState,
+      [category]: !prevState[category],
+    }));
+  };
+
+  const handleAddToOrder = (itemName: string, price: number) => {
+    setOrderItems((prevOrder) => {
+      const updatedOrder = { ...prevOrder };
+      if (updatedOrder[itemName]) {
+        updatedOrder[itemName].quantity += 1;
+      } else {
+        updatedOrder[itemName] = { quantity: 1, price };
+      }
+      return updatedOrder;
+    });
+  };
+  const handleIncreaseQuantity = (itemName: string) => {
+    setOrderItems((prevOrder) => ({
+      ...prevOrder,
+      [itemName]: {
+        ...prevOrder[itemName],
+        quantity: prevOrder[itemName].quantity + 1,
+      },
+    }));
+  };
+
+  const handleDecreaseQuantity = (itemName: string) => {
+    setOrderItems((prevOrder) => {
+      const updatedOrder = { ...prevOrder };
+      if (updatedOrder[itemName].quantity > 1) {
+        updatedOrder[itemName].quantity -= 1;
+      } else {
+        delete updatedOrder[itemName];
+      }
+      return updatedOrder;
+    });
+  };
+
+  const calculateTotal = () => {
+    return Object.values(orderItems).reduce(
+      (total, item) => total + item.quantity * item.price,
+      0
+    );
+  };
+
+  const handleDeleteItem = (itemName: string) => {
+    setOrderItems((prevOrder) => {
+      const updatedOrder = { ...prevOrder };
+      delete updatedOrder[itemName];
+      return updatedOrder;
+    });
+  };
+
+  const scrollToSection = (category: string) => {
+    const section = sectionRefs.current[category];
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+
+      // Open the collapsible
+      setOpenSections((prevState) => ({
+        ...prevState,
+        [category]: true,
+      }));
+    }
+  };
 
   return (
     <section className="container relative flex flex-col">
@@ -85,9 +170,11 @@ const RestaurantDetails = ({
               {" "}
               <ImageIcon /> View Photo
             </DialogTrigger>
-
+            {/* image gallery */}
             <DialogContent className=" bg-green-500" id="dialog-description">
-              <DialogTitle className="hidden">Are you absolutely sure?</DialogTitle>
+              <DialogTitle className="hidden">
+                Are you absolutely sure?
+              </DialogTitle>
               <div className="carousel ">
                 <div className="carousel__item carousel__item--left">
                   <Image
@@ -201,6 +288,7 @@ const RestaurantDetails = ({
                   key={id}
                   className="border-1 border-black 
                   border-transparent hover:text-primary transition-all duration-300 ease-in-out text-xl h-full"
+                  onClick={() => scrollToSection(menu.category)}
                 >
                   {menu.category}
                 </TabsTrigger>
@@ -214,17 +302,35 @@ const RestaurantDetails = ({
         </Tabs>
       </>
       <div className="grid grid-cols-1 md:grid-cols-2  gap-8 py-4">
-        <div className="md:row-span-2 rounded-2xl flex flex-col gap-4 p-2 flex-grow border-2 border-primary shadow-md shadow-white/30">
+        <div className="md:row-span-2 rounded-2xl flex flex-col gap-4 p-2 flex-grow border border-primary shadow-md shadow-white/30">
           {menuLists.map((menu, id) => {
             return (
-              <div key={id}>
-                <Collapsible>
-                  <CollapsibleTrigger className="flex justify-between items-center w-full p-4 bg-transparent">
+              <div
+                key={id}
+                ref={(el) => {
+                  sectionRefs.current[menu.category] = el;
+                }}
+              >
+                <Collapsible
+                  key={menu.category}
+                  open={openSections[menu.category]}
+                >
+                  <CollapsibleTrigger
+                    className="flex justify-between items-center w-full py-2 bg-transparent h-full"
+                    // onClick={() => scrollToSection(menu.category)}
+                  >
                     <h1 className="text-lg md:text-xl font-bold cursor-pointer">
                       {menu.category}
                     </h1>
 
-                    <ChevronsDown className="hover:animate-bounce" />
+                    <div>
+                      {" "}
+                      {openSections[menu.category] ? (
+                        <FaSortDown />
+                      ) : (
+                        <FaSortUp />
+                      )}
+                    </div>
                   </CollapsibleTrigger>
                   <CollapsibleContent>
                     <Table>
@@ -256,7 +362,15 @@ const RestaurantDetails = ({
                                 {menu_item.price}
                               </TableCell>
                               <TableCell className="w-1/6">
-                                <Button className="rounded-full ">
+                                <Button
+                                  className="rounded-full "
+                                  onClick={() =>
+                                    handleAddToOrder(
+                                      menu_item.name,
+                                      menu_item.price
+                                    )
+                                  }
+                                >
                                   <Plus className="h-full w-full font-bold" />
                                 </Button>
                               </TableCell>
@@ -271,13 +385,64 @@ const RestaurantDetails = ({
             );
           })}
         </div>
-        <div className="md:row-span-1 bg-green-100  md:flex md:flex-col flex-row   gap-8 justify-center flex-grow ">
+        <div className="md:row-span-1   md:flex md:flex-col flex-row    justify-center flex-grow border border-primary shadow-md shadow-white/30 rounded-2xl overflow-hidden">
           <div className="w-full h-auto bg-primary p-2 text-center text-white">
             <h1 className="text-3xl font-bold ">Order Summary</h1>
           </div>
-          <div className="flex flex-col gap-2 w-full px-8 justify-center h-auto my-4">
-            <Button className="w-full text-xl">Checkout</Button>
-            <span className="text-sm md:text-xl text-center ">
+          <div className="flex flex-col gap-2 w-full px-8 justify-center h-auto ">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="text-left">Item</TableHead>
+                  <TableHead className="text-center">Price</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Total</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Object.entries(orderItems).map(
+                  ([itemName, { quantity, price }]) => (
+                    <TableRow key={itemName}>
+                      <TableCell className="text-left">{itemName}</TableCell>
+                      <TableCell className="text-center">Rs. {price}</TableCell>
+                      <TableCell className="text-center flex items-center justify-center space-x-4">
+                        {" "}
+                        <Button
+                          onClick={() => handleDecreaseQuantity(itemName)}
+                          className="text-sm px-2 py-1 border rounded"
+                        >
+                          -
+                        </Button>
+                        <span>{quantity}</span>
+                        <Button
+                          onClick={() => handleIncreaseQuantity(itemName)}
+                          className="text-sm px-2 py-1 border rounded"
+                        >
+                          +
+                        </Button>
+                      </TableCell>
+                      <TableCell className="text-right">
+                        Rs. {quantity * price}
+                      </TableCell>
+                      <TableCell className="text-center w-5">
+                        <Button
+                          onClick={() => handleDeleteItem(itemName)}
+                          className="text-sm    text-black bg-transparent cursor-pointer hover:text-red-500 hover:bg-transparent"
+                        >
+                          <CircleX />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  )
+                )}
+              </TableBody>
+            </Table>
+            <div className="flex justify-between font-bold text-xl px-4">
+              <span>Total:</span>
+              <span>Rs. {calculateTotal()}</span>
+            </div>
+            <Button className=" text-xl ">Checkout</Button>
+            <span className="text-sm  text-center ">
               Delivery Charge will be calculated on next step
             </span>
           </div>
