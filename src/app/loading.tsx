94d127e3b641fec92c6a931f79a893407e58ebe0
Babina081 <babitamoo333@gmail.com>
2024-08-26 14:12:28 +0545

    add useeffect timeout hook

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,30 +1,43 @@
-"use cient";
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-const Loading = () => {
+export default function Loading() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous operation
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // Adjust as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="load  h-[100vh] w-[100vw] flex flex-col justify-center items-center overflow-hidden">
-      <div className="hamburger">
-        <div className="top-bun"></div>
-        <div className="pickle"></div>
-        <div className="pickle"></div>
-        <div className="tomato">
-          <div></div>
+    <>
+      {loading && (
+        <div className="load  h-[100vh] w-[100vw] flex flex-col justify-center items-center overflow-hidden">
+          <div className="hamburger">
+            <div className="top-bun"></div>
+            <div className="pickle"></div>
+            <div className="pickle"></div>
+            <div className="tomato">
+              <div></div>
+            </div>
+            <div className="tomato">
+              <div></div>
+            </div>
+
+            <div className="cheese"></div>
+            <div className="cheese"></div>
+
+            <div className="beef"></div>
+
+            <div className="bottom-bun"></div>
+          </div>
+          <h1 className="ham-h1">Loading...</h1>
         </div>
-        <div className="tomato">
-          <div></div>
-        </div>
-
-        <div className="cheese"></div>
-        <div className="cheese"></div>
-
-        <div className="beef"></div>
-
-        <div className="bottom-bun"></div>
-      </div>
-      <h1 className="ham-h1">Loading...</h1>
-    </div>
+      )}
+    </>
   );
-};
-
-export default Loading;
+}
